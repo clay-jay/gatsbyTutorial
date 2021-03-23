516b19af2df648bdc81fbd1bf2ddbf3ee6ee34ce
clay-jay <youkozel@gmail.com>
2021-03-23 15:42:28 +0300

    Initial commit

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../components/header'
+
+export default function About(){
+    return(
+        <div>
+            <Header headerText="About Gatsby" />
+            <Header headerText="Another Header" />
+            <p>That is really something</p>
+        </div>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,16 @@
 import React from "react"
+import {Link} from "gatsby"
+import Header from "../components/header"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div style={{color: "lightblue"}}>
+      <Header headerText="Hello world!"/>
+      <Link to="/contract/">Contract</Link>
+      <p>SomeText</p>
+      <Link to="/about/">About</Link>
+      <p>SomeText</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </div>
+  )
 }
